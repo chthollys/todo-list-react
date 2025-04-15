eac7549348029b93ede7d944756dcedb85e63cad
chthollys <jason130105@gmail.com>
2025-04-15 09:00:42 +0700

    added tree components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,11 @@
 import React, { useState, useEffect } from "react";
-import ToDoItem from "./ToDoItem";
-// let currentIndex = 0;
+import ToDoItem from "./ToDoItem.jsx";
+import InputArea from "./InputArea.jsx";
 
 function App() {
 
   const [inputList, setInputList] = useState([]);
   const [inputValue, setInputValue] = useState("");
-
-  // const onChangeInput = (e) => {
-  //   const { value } = e.target;
-  //   // console.log(value);
-  //   setInputValue(value);
-  //   setInputList((prevList) => {
-  //     return [
-  //       ...prevList.slice(0, currentIndex),
-  //       value,
-  //     ] 
-  //   });    
-  // };
-
-  // const onFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   // console.log(inputValue);
-  //   setInputList((prevList) => {
-  //     return [
-  //       ...prevList.slice(0, currentIndex - 1),
-  //       inputValue
-  //     ];
-  //   })
-  //   setInputValue("");
-  //   currentIndex ++;
-  // };
 
   const onChangeInput = (e) => {
     setInputValue(e.target.value);
@@ -43,40 +18,18 @@ function App() {
     setInputValue("");
   };
 
-  // useEffect(() => {
-  //   console.log(inputValue);
-  // }, [inputValue])
-
-  // useEffect(() => {
-  //   console.log(inputList);
-  // }, [inputList])
-
-  // useEffect(() => {
-  //   console.log(listHTML);
-  // }, [listHTML])
-
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <form onSubmit={onFormSubmit} className="form">
-        <input
-         type="text"
-         placeholder="Add a new item..."
-         onChange={onChangeInput}
-         value={inputValue}
-        />
-        <button type="submit">
-          <span>Add</span>
-        </button>
-      </form>
+      <InputArea onFormSubmit={onFormSubmit} onChangeInput={onChangeInput} inputValue={inputValue}/>
       <div>
         <ul>
           {inputList.map((value, index) => (
-            <ToDoItem key={index} item={value} />
+            <ToDoItem key={index} item={value} included={true} />
           ))}
-          {inputValue && <ToDoItem item={inputValue} />}
+          {inputValue && <ToDoItem included={false} item={inputValue} />}
         </ul>
       </div>
     </div>
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
-function ToDoItem ( { item } ) {
+function ToDoItem ( { item, included } ) {
 	const [isCompleted, setIsCompleted] = useState(false);
 	const onClickList = () => {
 		setIsCompleted((prev) => !prev);
 	}
-    return (
-      <li
-				onClick={onClickList}
-				style={{textDecoration : isCompleted ? "line-through" : "none"}}
-			>
-				{item}
-			</li>
-    );
+	const errorMessage = (mess) => alert(mess);
+	return (
+		<li
+			onClick={included ? onClickList : () => errorMessage("Please complete the input process")}
+			style={{textDecoration : isCompleted ? "line-through" : "none"}}
+		>
+			{item}
+		</li>
+	);
 };
 
 export default ToDoItem;
